e73c8a5c28006a5e8f20c6d3a3959f92866d5366
Magn3tism <chapagainsakshyam@gmail.com>
2022-11-27 11:59:40 +0545

    Create bot

--- main.js
+++ main.js
@@ -28,9 +28,28 @@ const Player = (name, piece, score, elememt) => {
 
 const player1 = Player("Player X", "X", 0, p1);
 const player2 = Player("Player O", "O", 0, p2);
+const bot = Player("Bot", "O", 0, p2);
 
 const gameController = (() => {
   let player = player1;
+
+  // for (let i = 0; i < 9; i++) {
+  //   buttons[i].addEventListener("click", () => {
+  //     if (!buttons[i].textContent) {
+  //       buttons[i].textContent = player.getPiece();
+  //       buttons[i].classList.add(player.getPiece());
+  //       pieces[i] = player.getPiece();
+
+  //       if (!checkWin(player)) {
+  //         if (player.getPiece() === "X") {
+  //           player = player2;
+  //         } else player = player1;
+  //         message.textContent = `${player.getName()}'s Turn`;
+  //         checkDraw();
+  //       }
+  //     }
+  //   });
+  // }
 
   for (let i = 0; i < 9; i++) {
     buttons[i].addEventListener("click", () => {
@@ -40,11 +59,19 @@ const gameController = (() => {
         pieces[i] = player.getPiece();
 
         if (!checkWin(player)) {
-          if (player.getPiece() === "X") {
-            player = player2;
-          } else player = player1;
-          message.textContent = `${player.getName()}'s Turn`;
-          checkDraw();
+          if (!checkDraw()) {
+            while (true) {
+              let position = Math.floor(Math.random() * 8);
+              console.log(position);
+              if (!buttons[position].textContent) {
+                buttons[position].textContent = "O";
+                buttons[position].classList.add("O");
+                pieces[position] = "O";
+                checkWin(bot);
+                break;
+              }
+            }
+          }
         }
       }
     });
@@ -100,6 +127,7 @@ const gameController = (() => {
       buttons.forEach((button) => (button.disabled = true));
       player.increaseScore();
       play.disabled = false;
+      reset.disabled = false;
       return 1;
     }
   };
@@ -113,6 +141,8 @@ const gameController = (() => {
     if (count === 9) {
       message.textContent = "Draw";
       play.disabled = false;
+      reset.disabled = false;
+      return true;
     }
   };
 })();
@@ -129,6 +159,7 @@ const gameFunction = (() => {
     message.textContent = "Player X's turn";
     play.textContent = "Play Again";
     play.disabled = true;
+    reset.disabled = true;
     pieces = pieces.map((piece) => "");
   });
 
